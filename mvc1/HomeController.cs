b090dd5118bef2de5a805021f4a584bcf1bdb468
granlite <65130690+granlite@users.noreply.github.com>
2020-09-15 21:44:22 +0400

    Add files via upload

---
+++ mvc1/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using mvc1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "description";
+
+            return View();
+        }
+        public ActionResult Contact(string name, int id)
+        {
+            Employer john = new Employer() { Name = name, Id = id };
+            return Json(john, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
